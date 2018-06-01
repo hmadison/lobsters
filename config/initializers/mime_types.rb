36ded15c44cfc7326bd2cde26f097d9ae2bf6c70
Hunter Madison <hmadison@noreply.github.com>
2018-05-31 20:43:41 -0400

    Support and parse ld json

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,6 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+# Mime::Type.register_alias "text/html", :iphone
+Mime::Type.register 'application/ld+json', :ld_json
+ActionDispatch::Request.parameter_parsers[:ld_json] = -> (body) { JSON.parse(body) }
