37051d93946205acfbf4cf8a7cf606cf3cfca2a1
Peter Bhat Harkins <peter@push.cx>
2020-01-22 08:47:22 -0600

    rm index migration - our mariadb is too old

--- db/migrate/20191227231938_add_full_text_index_to_story_cache.rb
+++
@@ -1,7 +0,0 @@
-class AddFullTextIndexToStoryCache < ActiveRecord::Migration[5.2]
-  def change
-    execute <<~SQL
-    CREATE OR REPLACE FULLTEXT INDEX stories_story_cache ON stories(story_cache)
-    SQL
-  end
-end
